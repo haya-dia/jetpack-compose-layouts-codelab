14bea2c63c6a4e78dfe7e934554a5895be67700e
haya_dia <hirovabo@yahoo.co.jp>
2021-03-15 01:15:15 +0900

    Add photographer text

--- app/src/main/java/com/example/jetpackcomposelayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelayoutscodelab/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.jetpackcomposelayoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposelayoutscodelab.ui.theme.JetpackComposeLayoutsCodelabTheme
 
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeLayoutsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerCard()
                 }
             }
         }
@@ -25,14 +26,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun PhotographerCardPreview() {
     JetpackComposeLayoutsCodelabTheme {
-        Greeting("Android")
+        PhotographerCard()
     }
 }
