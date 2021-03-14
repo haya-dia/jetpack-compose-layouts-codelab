df3fa1e38af7ea63fb0dd645fa26c655a3890187
haya_dia <hirovabo@yahoo.co.jp>
2021-03-15 01:18:09 +0900

    Add circle surface

--- app/src/main/java/com/example/jetpackcomposelayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelayoutscodelab/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposelayoutscodelab.ui.theme.JetpackComposeLayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,10 +32,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PhotographerCard() {
-    Column {
-        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
