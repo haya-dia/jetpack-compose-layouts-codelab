d8ce3f2134e55f54cec6af375366fbc0f8e9da3b
haya_dia <hirovabo@yahoo.co.jp>
2021-03-15 01:53:07 +0900

    Add body padding

--- app/src/main/java/com/example/jetpackcomposelayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelayoutscodelab/MainActivity.kt
@@ -52,7 +52,7 @@ fun LayoutsCodelab() {
             )
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding))
+        BodyContent(Modifier.padding(innerPadding).padding(10.dp))
     }
 }
 
